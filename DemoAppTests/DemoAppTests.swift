2c5c847a1d05012d1a532227cc14f488d5433808
parvind16 <parvind.bhatt@gmail.com>
2019-03-27 16:24:21 +0530

    unit test add

--- DemoAppTests/DemoAppTests.swift
+++ DemoAppTests/DemoAppTests.swift
@@ -10,25 +10,66 @@ import XCTest
 @testable import DemoApp
 
 class DemoAppTests: XCTestCase {
-
+    
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-
+    
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
+    
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
-
+    
+    func testWeatherForecast() {
+        testForecastForURL(host: "api.apixu.com", fileName: "Forecast.json")
+    }
+    func testCurrentForecast() {
+        testForecastForURL(host: "api.apixu.com", fileName: "Current.json")
+    }
+    
+    func testForecastForURL(host: String, fileName: String) {
+        
+        XCHttpStub.request(withPathRegex: host, withResponseFile: fileName)
+        let responseExpectation = expectation(description: "return expected data of List")
+        
+        APIManager.weatherApi(serviceName: Constant.API.apiURL(Constant.API.forecastWeather), parameters: nil) { (response, error) in
+            
+            if error != nil {
+                XCTAssertNotNil(error, "error: Expectation fulfilled with error")
+                responseExpectation.fulfill()
+                return
+            }
+            
+            guard let _ = response else {
+                return
+            }
+            
+            if response is WeatherDataModel{
+                let weather = WeatherViewModel(weatherDataModel: response as! WeatherDataModel)
+                XCTAssertNotNil(weather, "list: expected result achived")
+                XCTAssertNil(error, "error: Expectation fulfilled with error")
+                responseExpectation.fulfill()
+                
+            }
+        }
+        
+        waitForExpectations(timeout: 50) { error in
+            if let error = error {
+                XCTAssertNotNil(error, "Failed to get response from list webservice")
+            }
+        }
+    }
+    
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
         }
     }
-
+    
 }
+
---
+++ Podfile
@@ -0,0 +1,22 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'DemoApp' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+  
+  # Pods for DemoApp
+  pod 'OHHTTPStubs/Swift'
+  
+  target 'DemoAppTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+  
+  target 'DemoAppUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+  
+end
+
